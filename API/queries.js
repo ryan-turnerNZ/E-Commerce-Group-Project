02172fb1d97bfe511a95c1a360599976c32d2014
Ryan Turner <turnerryan1@ecs.vuw.ac.nz>
2019-10-20 17:45:30 +1300

    Created a register user function using bcrypt

--- API/queries.js
+++ API/queries.js
@@ -1,9 +1,25 @@
-require('dotenv').config()
-const { Pool } = require('pg')
+require('dotenv').config();
+const { Pool } = require('pg');
 const connectionString = process.env.connectionString;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const pool = new Pool({
     connectionString: connectionString,
     ssl: true,
-})
+});
 
+const registerUser = (request, response) => {
+    const {username, plainTextPass} = request.body;
+    bcrypt.hash(plainTextPass, saltRounds, function(err, hash) {
+        pool.query('INSERT INTO users (username, hash) VALUES ($1, $2)', [username, hash], (error, results) => {
+            if(error){
+                response.status(400).send('Failed to add Task');
+                throw error;
+            }
+            response.status(201).send('Task added');
+        })
+    });
+
+
+}
