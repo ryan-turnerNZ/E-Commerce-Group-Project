a1ae159e78e8c22b6ed9e2fb11bbaebbe33f9f6a
Ryan Turner <turnerryan1@ecs.vuw.ac.nz>
2019-10-24 15:54:13 +1300

    Creating Login service. Need to change authentication endpoint

--- Angular/Rentflix/src/app/services/login_service/login-service.service.ts
+++ Angular/Rentflix/src/app/services/login_service/login-service.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginServiceService {
+export class LoginServiceService{
 
   constructor(private http: HttpClient) {
 
+  }
+
+  async authenticateLogin(username, password) {
+    this.http.get('https://dashboard.heroku.com/apps/rent-flix/');
   }
 }
