62793c5dc309334cb0f5ee804dffc34a8ca6b70a
Annisha Akosah <akosahanni@ecs.vuw.ac.nz>
2019-10-26 12:04:09 +1300

    Connect to TMDB Api and retrieve newest/popular movies on landing page

--- Angular/Rentflix/src/app/pages/home/home.component.ts
+++ Angular/Rentflix/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TMDBService } from '../../services/tmdb-service/tmdb.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  newReleases: any[];
 
-  constructor() { }
+  constructor(private TMDBService: TMDBService,) { }
 
   ngOnInit() {
+    this.TMDBService.discoverNewest();
+    this.TMDBService.discoverPopular();
+  }
+
+  public getNewReleases() {
+    if(this.TMDBService.getNewest()) {
+      this.newReleases = this.TMDBService.getNewest().slice(0,4)
+    }
+
+    // remove any results that have no poster
+    if (this.newReleases) return this.newReleases.filter(t => t.poster_path != null);
   }
 
 }
