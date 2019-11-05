b0ac4417d1cf4ef358bcad35a177ea53fa3daa35
Vishal <vishaljr@hotmail.com>
2019-11-05 17:53:51 +1300

    orders working
    still need to fix total prices

--- Angular/Rentflix/src/app/pages/AccountPages/orders/orders.component.ts
+++ Angular/Rentflix/src/app/pages/AccountPages/orders/orders.component.ts
@@ -12,6 +12,7 @@ export class OrdersComponent implements OnInit {
 
   constructor(private loginService: LoginService, private orderService: OrdersService, private moviedb: TMDBService) { }
   private orders = [];
+  mapOrders;
   ngOnInit() {
     this.moviedb.clearMovieArray();
     this.getOrders();
@@ -22,22 +23,43 @@ export class OrdersComponent implements OnInit {
       res.subscribe(data => {
         const response = (data as {results: any});
         this.orders = response.results;
-        console.log(this.orders);
-        const d = new Date(this.orders[0].purchase_date);
-        console.log(d.getUTCDate());
+        this.convertCart();
       });
     });
   }
   convertCart() {
     this.moviedb.clearMovieArray();
-    this.orders.forEach(async  o => {
-      await this.moviedb.getMovieFromID2(o.purchase_id);
+    this.orders.forEach(async val => {
+      await this.moviedb.getMovieFromID3(val);
     });
+
   }
+
   getArray() {
-    return this.moviedb.getMovieArray();
+    const temp = this.moviedb.getMovieArray2();
+    this.mapOrders = this.groupBy(temp, time => time.timeAdded);
+    return this.mapOrders;
   }
   getTotalCost() {
     return this.moviedb.getTotalCartCost();
+  }
+  public getPrice(date) {
+    const year = date.substring(0, 4);
+    console.log(year);
+    if (year >= 2019) { return '$8.99'; } else if (year <= 2018 && year > 2015) { return '$5.99'; }
+    return '$3.99';
+  }
+   groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+      const key = keyGetter(item);
+      const collection = map.get(key);
+      if (!collection) {
+        map.set(key, [item]);
+      } else {
+        collection.push(item);
+      }
+    });
+    return map;
   }
 }
