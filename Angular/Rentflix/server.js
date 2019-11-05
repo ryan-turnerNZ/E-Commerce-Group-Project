31b10160bdc56b45e512cf456cffeb92da1c5c22
Annisha Akosah <akosahanni@ecs.vuw.ac.nz>
2019-11-05 23:04:58 +1300

    Update package.json for deployment to heroku

--- Angular/Rentflix/server.js
+++ Angular/Rentflix/server.js
@@ -1,9 +1,14 @@
-const path = require("path");
-const express = require("express");
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
+
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/angular-build'));
 app.get('/*', function(req,res){
 res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
-});
+})
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
